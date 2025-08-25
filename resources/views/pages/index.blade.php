8539b65611164169f3772f7d3d77781570532165
MohammedMustafa <49589909+MohammedMoo@users.noreply.github.com>
2025-08-25 14:12:25 +0300

    tags

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,4 +4,5 @@
 
 @section('content')
     @include('partials.index.main')
+    @include('partials.index.about')
 @endsection
---
+++ resources/views/partials/index/about.blade.php
@@ -0,0 +1,39 @@
+<section id="aboutSection">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="align-items-center d-flex imageAndTitle justify-content-around position-relative">
+                <img src="{{ asset('assets/imgs/Group237615.png')}}" alt="">
+                <div class="aboutTitle position-absolute">
+                    About <br> US
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div class="aboutSectiontwo">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque libero voluptatibus hic accusamus
+                    natus ratione nihil mollitia commodi aspernatur cumque, sint possimus eveniet sit aperiam vitae
+                    obcaecati dolorem tempora? Libero?</p>
+                <div class="numLineTxtSection">
+                    <div class="numLineTxt">
+                        <div class="num">+200</div>
+                        <div class="line"></div>
+                        <div class="txt">Employee</div>
+                    </div>
+                    <div class="numLineTxt">
+                        <div class="num">+500</div>
+                        <div class="line"></div>
+                        <div class="txt">Projects</div>
+                    </div>
+                    <div class="numLineTxt">
+                        <div class="num">+300</div>
+                        <div class="line"></div>
+                        <div class="txt">Clients</div>
+                    </div>
+                </div>
+                <p class="2dis">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nisi nam amet aliquam rerum itaque
+                    nobis odio explicabo ipsum? Non eius fugiat corporis incidunt, ullam eligendi praesentium libero
+                    accusantium tempore.</p>
+            </div>
+        </div>
+    </div>
+</section>
