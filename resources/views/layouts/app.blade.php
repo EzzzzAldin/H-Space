e7c73bc5e5abc2a37b24d3876a5227830609c84c
MohammedMustafa <49589909+MohammedMoo@users.noreply.github.com>
2025-08-26 14:06:32 +0300

    div

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,7 @@
     <title>@yield('title', config('app.name'))</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -20,8 +19,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Sora:wght@100..800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.min.css" />
 
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/custome.css') }}" />
--- resources/views/partials/index/services.blade.php
+++ resources/views/partials/index/services.blade.php
@@ -1,11 +1,61 @@
 <section class="services container">
     <div class="title">Services</div>
     <div class="largTitle">What we do offer</div>
-    <div class="owlSection">
+    <div class="owlSection1 owl-carousel owl-theme">
         <div class="item">
-            <img src="" alt="">
-            <div class="dis">Web<br>Development</div>
-            <div class="icon"></div>
+            <div class="">
+                <img src="" alt="">
+                <div class="dis">Web<br>Development</div>
+                <div class="icon"></div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="">
+                <img src="" alt="">
+                <div class="dis">Web<br>Development</div>
+                <div class="icon"></div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="">
+                <img src="" alt="">
+                <div class="dis">Web<br>Development</div>
+                <div class="icon"></div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="">
+                <img src="" alt="">
+                <div class="dis">Web<br>Development</div>
+                <div class="icon"></div>
+            </div>
+        </div>
+        <div class="item">
+            <div class="">
+                <img src="" alt="">
+                <div class="dis">Web<br>Development</div>
+                <div class="icon"></div>
+            </div>
         </div>
     </div>
 </section>
+@push('scripts')
+    <script>
+        $('.owlSection1').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 3
+                },
+                1000: {
+                    items: 4
+                }
+            }
+        })
+    </script>
+@endpush
