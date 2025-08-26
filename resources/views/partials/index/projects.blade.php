5835b8f4f9b16398c807a95db052fe05338c4905
EzzzzAldin74 <zezomomhamed74@gmail.com>
2025-08-26 16:55:29 +0300

    section clients part 3

---
+++ resources/views/partials/index/projects.blade.php
@@ -0,0 +1,22 @@
+<div class="main-projects">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-4">
+                <div class="main-info">
+                    <h2 class="main-title text-center">Our Customers</h2>
+                    <p class="text-center mt-3">Our Work</p>
+                </div>
+                <div class="d-grid gap-2">
+                    <button class="btn btn-app" type="button">Web Application</button>
+                    <button class="btn btn-app" type="button">Web Application</button>
+                    <button class="btn btn-app" type="button">Web Application</button>
+                    <button class="btn btn-app" type="button">Web Application</button>
+                </div>
+
+            </div>
+            <div class="col-12 col-md-8">
+                Gallery
+            </div>
+        </div>
+    </div>
+</div>
