ea56dd9555d02d4f05efaec3b38c4d12d16f2eab
MohammedMustafa <49589909+MohammedMoo@users.noreply.github.com>
2025-08-31 13:50:25 +0300

    dev

--- resources/views/partials/index/projects.blade.php
+++ resources/views/partials/index/projects.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12 col-md-4">
                 <div class="main-info">
-                    <h2 class="main-title text-center">Our Customers</h2>
+                    <h2 class="main-title text-center">Projects</h2>
                     <p class="text-center mt-3">Our Work</p>
                 </div>
                 <div class="d-grid gap-2">
@@ -17,20 +17,48 @@
             <div class="col-12 col-md-8">
                 <div class="row">
                     <div class="col-md-3">
-                        <div class="imageL"><img src="" alt=""></div>
-                        <div class="image"><img src="" alt=""></div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="imageL"><img class="w-100" src="{{ asset('assets/imgs/Bitmap.png') }}"
+                                        alt=""></div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="image"><img class="w-100" src="{{ asset('assets/imgs/Bitmap2.png') }}"
+                                        alt=""></div>
+                            </div>
+                        </div>
                     </div>
                     <div class="col-md-9">
-                        <div class="row">
-                            <div class="col-md-3"><img src="" alt=""></div>
-                            <div class="col-md-3"><img src="" alt=""></div>
-                            <div class="col-md-5"><img src="" alt=""></div>
+                        <div class="row the3images">
+                            <div class="col-md-3 h-100"><img class="w-100" src="{{ asset('assets/imgs/Bitmap1.png') }}"
+                                    alt="">
+                            </div>
+                            <div class="col-md-3 h-100"><img class="w-100" src="{{ asset('assets/imgs/Bitmap3.png') }}"
+                                    alt="">
+                            </div>
+                            <div class="col-md-6 h-100"><img class="w-100" src="{{ asset('assets/imgs/Bitmap4.png') }}"
+                                    alt="">
+                            </div>
                         </div>
                         <div class="row">
-                            
+                            <div class="col-md-5">
+                                <div class="row">
+                                    <div class="col-md-12 h-100"><img class="w-100"
+                                            src="{{ asset('assets/imgs/Bitmap5.png') }}" alt=""></div>
+                                    <div class="col-md-12 h-100"><img class="w-100"
+                                            src="{{ asset('assets/imgs/Bitmap6.png') }}" alt=""></div>
+                                </div>
+                            </div>
+                            <div class="col-md-7 h-100"><img class="w-100" src="{{ asset('assets/imgs/Bitmap7.png') }}"
+                                    alt="">
+                            </div>
                         </div>
                     </div>
-
+                    <div class="col-md-12">
+                        <div class="showMore">
+                            <button>Show more</button>
+                        </div>
+                    </div>
                 </div>
 
             </div>
