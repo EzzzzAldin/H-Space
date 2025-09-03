c890a42ae5184ebd8d2583b338ee79b8949e7f6e
EzzzzAldin74 <zezomomhamed74@gmail.com>
2025-09-03 15:29:20 +0300

    edits style

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,18 +3,11 @@
 @section('title', 'Home')
 
 @section('content')
-    <div class="mainBackground position-relative">
-        <img class="h-100 position-absolute w-100 z-0" src="{{ asset('assets/imgs/Homebghgfh.png') }}" alt="" style="top: 10rem;">
-        <div class="position-relative z-1">
-            @include('partials.index.main')
-            @include('partials.index.about')
-            @include('partials.index.services')
-            @include('partials.index.customers')
-            @include('partials.index.projects')
-            @include('partials.index.team')
-            @include('partials.index.contactus')
-        </div>
-    </div>
-
-
+    @include('partials.index.main')
+    @include('partials.index.about')
+    @include('partials.index.services')
+    @include('partials.index.customers')
+    @include('partials.index.projects')
+    @include('partials.index.team')
+    @include('partials.index.contactus')
 @endsection
